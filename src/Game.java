72bd774a0e43bb6c885d85eb536a52e0f7e626fc
ManuellZB <nuelliiee@gmail.com>
2024-12-05 11:25:57 +0800

    Add files via upload

---
+++ src/Game.java
@@ -0,0 +1,6 @@
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Life Challenge Game!");
+        // Add game initialization logic here
+    }
+}
---
+++ src/model/GameModel.java
@@ -0,0 +1,5 @@
+package model;
+
+public class GameModel {
+    // Model logic here
+}
